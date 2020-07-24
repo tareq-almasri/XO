beb15d8ca3207bc4363f1e625476a38a2cf9fcec
tareq-almasri <talmasreq@gmail.com>
2020-07-24 12:45:32 +0200

    Updates

---
+++ precache-manifest.05068e00f1ada224420add7132d93bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933ed5ef4be5995d00825b2b9ab64efa",
+    "url": "/XO/index.html"
+  },
+  {
+    "revision": "c9aca34e5f570f9f350c",
+    "url": "/XO/static/css/main.77442e12.chunk.css"
+  },
+  {
+    "revision": "fcac614d45f3ce4fe1b2",
+    "url": "/XO/static/js/2.275f021c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/XO/static/js/2.275f021c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9aca34e5f570f9f350c",
+    "url": "/XO/static/js/main.b127cf4f.chunk.js"
+  },
+  {
+    "revision": "e13170b15c9721002ec8",
+    "url": "/XO/static/js/runtime-main.507c5d6e.js"
+  }
+]);
